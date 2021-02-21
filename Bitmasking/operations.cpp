ebc9222f6cf2a356a9b1d3c1667d2adbe511e7a3
proghead00 <susnatadj.rocks@gmail.com>
2021-02-21 12:16:00 +0530

    bitmasking

---
+++ Bitmasking/operations.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+int ith_bit(int n, int i)
+{
+
+    return (n & (1 << i)) > 0 ? 1 : 0;
+}
+
+int set_ith_bit(int n, int i)
+{
+    return n | (1 << i);
+}
+
+int clear_ith_bit(int n, int i)
+{
+
+    int mask = ~(1 << i);
+    n = (n & mask);
+
+    return n;
+}
+
+int update_ith_bit(int n, int v, int i)
+{
+    int mask = ~(1 << i);
+
+    int cleared_n = n & mask;
+
+    n = cleared_n | (v << i);
+    return n;
+}
+
+int clear_range_bits(int n, int i)
+{
+    int mask = (-1 << i);
+    n = (n & mask);
+
+    return n;
+}
+
+int clear_i_to_j(int n, int i, int j)
+{
+    int ones = ~0;
+
+    int a = ones << j + 1;
+
+    int b = (1 << i) - 1;
+
+    int mask = a | b;
+
+    int ans = n & mask;
+
+    return ans;
+}
+
+int main()
+{
+
+    int n, i, j;
+
+    cin >> n >> i >> j;
+
+    int update = clear_i_to_j(n, i, j);
+
+    cout << update << endl;
+}
