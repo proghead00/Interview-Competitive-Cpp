b88007ae46a80bfa5901e97ae4d3773f47d8a174
proghead00 <susnatadj.rocks@GMAIL.COM>
2021-05-02 20:47:14 +0530

    preorder

---
+++ Binary Tree/preorder.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int d)
+    {
+        // by default, left and right children are NULL
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+// tree will return address of root node
+node *buildTree()
+{
+    int d;
+    cin >> d;
+
+    if (d == -1)
+    {
+        return NULL;
+    }
+
+    node *root = new node(d);
+    root->left = buildTree();
+    root->right = buildTree();
+    return root;
+}
+
+void print(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    // otherwise, print root first followed by subtrees(chilren)
+    cout << root->data << " ";
+    print(root->left);
+    print(root->right);
+}
+
+int main()
+{
+    node *root = buildTree();
+    print(root);
+}
