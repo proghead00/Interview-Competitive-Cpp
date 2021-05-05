31fd9323d4dc90548dc6e012729427e2cd468e07
proghead00 <susnatadj.rocks@GMAIL.COM>
2021-05-05 22:17:41 +0530

    right view of binary tree

---
+++ Binary Tree/rightView.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *right;
+    node *left;
+
+    node(int d)
+    {
+        data = d;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+node *buildTree()
+{
+    int d;
+    cin >> d;
+
+    if (d == -1)
+    {
+        return NULL;
+    }
+
+    node *root = new node(d);
+    root->left = buildTree();
+    root->right = buildTree();
+    return root;
+}
+
+void rightView(node *root, int level, int &maxLevel)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    if (maxLevel < level)
+    {
+        //discovered a new level
+        cout << root->data << " ";
+        maxLevel = level;
+    }
+
+    rightView(root->right, level + 1, maxLevel);
+    rightView(root->left, level + 1, maxLevel);
+}
+
+int main()
+{
+
+    node *root = buildTree();
+
+    int maxLevel = -1;
+
+    rightView(root, 0, maxLevel);
+}
