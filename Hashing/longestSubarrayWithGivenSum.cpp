fbbe83cbc4c3c34715ef3c3c8e960a936dde14e9
proghead00 <susnatadj.rocks@GMAIL.COM>
2021-05-28 23:45:30 +0530

    Longest subarray with

---
+++ Hashing/longestSubarrayWithGivenSum.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int longestSubarrayWithGivenSum(int arr[], int n) {
+	unordered_map<int, int> m;
+	int pre = 0, len = 0;
+
+	for (int i = 0; i < n; i++) {
+		pre += arr[i];
+
+		if (pre == 0)
+		{
+			len = max(len, i + 1);
+		}
+
+
+		if (m.find(pre) != m.end()) // if that element is already present
+		{
+			len = max(len, i - m[pre]);
+		}
+
+		else // first occurence of the element
+		{
+			m[pre] = i;
+		}
+	}
+
+	return len;
+
+}
+
+int main() {
+	int n;
+	cin >> n;
+
+	int arr[n];
+
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	int k = 4;
+	cout << longestSubarrayWithGivenSum(arr, n) << " ";
+
+
+}
