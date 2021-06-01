59e6227bf1437e7625675f43f3db7b8347ba11f9
proghead00 <susnatadj.rocks@GMAIL.COM>
2021-06-01 22:13:36 +0530

    hashing challenges

---
+++ Hashing -Tries Challenges/existsOrNot.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		int l;
+		cin >> l;
+
+		map<int, bool> hm;
+		int num;
+
+		for (int j = 0; j < l; j++) {
+			cin >> num;
+			hm[num] = 1;
+		}
+
+		int q;
+		cin >> q;
+		int query;
+
+		for (int k = 0; k < q; k++) {
+			cin >> query;
+			if (hm.count(query) > 0) {
+				cout << "Yes" << endl;
+			} else {
+				cout << "No" << endl;
+			}
+		}
+	}
+	return 0;
+}
---
+++ Hashing -Tries Challenges/verticalTraversal.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+
+class node {
+public:
+	int data;
+	node*left;
+	node*right;
+
+	node(int d) {
+		data = d;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+void verticalTrav(node*root, map<int, vector<int>>&m,  int hd) {
+	if (root == NULL) return;
+
+	m[hd].push_back(root->data);
+
+	verticalTrav(root->left, m, hd - 1);
+	verticalTrav(root->right, m, hd + 1);
+	return;
+
+}
+
+int main() {
+	node*root = new node(1);
+	root->left = new node(2);
+	root->right = new node(3);
+	root->left->left = new node(4);
+	root->left->right = new node(5);
+
+	root->right->left = new node(6);
+	root->right->right = new node(7);
+
+	root->right->right->right = new node(9);
+
+	root->left->right->right = new node(8);
+
+	map <int, vector<int>>m;
+	int hd = 0;
+	verticalTrav(root, m, hd);
+
+	for (auto p : m)
+	{
+		for (int x : p.second) {
+			cout << x << " ";
+
+		}
+		cout << endl;
+	}
+
+
+}
