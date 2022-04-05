4dc704a3334eee990797dc7eb0f651a0ac2dc23b
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-04-05 17:18:38 +0530

    Create completeString_CN.cpp

---
+++ Tries/completeString_CN.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+	char data;
+	unordered_map<char, Node*> mp;
+	bool isTerminal;
+
+	Node(char d) {
+		data = d;
+		isTerminal = false;
+	}
+};
+
+class Trie {
+public:
+
+	Node* root;
+
+	Trie() {
+		root = new Node('\0');
+	}
+
+	void insert(string word) {
+
+		Node* temp = root;
+
+		for (char w : word) {
+			if (temp->mp.count(w) == 0) {
+				Node* n = new Node(w);
+				temp->mp[w] = n;
+			}
+			temp = temp->mp[w];
+		}
+
+		temp-> isTerminal = true;
+	}
+
+	bool check(string word) {
+		Node *temp = root;
+		for (char w : word) {
+			if (temp->mp.count(w)) {
+				temp = temp->mp[w];
+				if (!temp->isTerminal) return false;
+			}
+			else return false;
+		}
+		return true;
+	}
+
+	string completeString(int n, vector<string> &a) {
+		for (string word : a) insert(word);
+
+		string res = "";
+
+		for (string word : a) {
+			if (check(word)) {
+				if (word.size() > res.size()) res = word;
+				else if (word.size() == res.size() and word < res) 	res = word;
+			}
+		}
+		if (res == "") return "None";
+		return res;
+	}
+};
